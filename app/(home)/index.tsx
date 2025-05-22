9f9f766a5aa10d91b42f011e94aea3a81d38ed88
lexerjohnahatchitsolutions <lexerjohnahatchitsolutions@gmail.com>
2025-05-22 08:31:14 +0800

    Updated Added dashboard nav buttons

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,7 +1,10 @@
-import { View, Text, Image, Dimensions } from "react-native";
+import { View, Text, Image, Dimensions, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButton from "@/components/buttons/CustomButton";
+import { useState } from "react";
+
+const screenWidth = Dimensions.get('window').width;
 
 export default function HomeScreen() {
   const router = useRouter();
@@ -34,12 +37,66 @@ export default function HomeScreen() {
         THIS IS HOME TEST
         </Text>
       </View>
-      <CustomButton
+      <View style={{ position: "absolute", bottom: 0}}>
+        <View style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", width: screenWidth, gap:20, borderTopColor: "#656262", borderTopWidth: 2,}}>
+          <TouchableOpacity onPress={() => console.log('Image clicked!')}>
+            <Image
+              source={require("../../assets/images/dashbordNavImg/Log.png")}
+              style={{
+                width: width * 0.15,
+                height: width * 0.15,
+                resizeMode: "contain",
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => console.log('Image clicked!')}>
+            <Image
+            source={require("../../assets/images/dashbordNavImg/Track.png")}  
+            style={{
+              width: width * 0.140,
+              height: width * 0.140,
+              resizeMode: "contain",
+            }}      
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => console.log('Image clicked!')}>
+            <Image
+            source={require("../../assets/images/dashbordNavImg/Home.png")}    
+            style={{
+              width: width * 0.140,
+              height: width * 0.140,
+              resizeMode: "contain",
+            }}    
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => console.log('Image clicked!')}>
+            <Image
+            source={require("../../assets/images/dashbordNavImg/Scan.png")}  
+            style={{
+              width: width * 0.140,
+              height: width * 0.140,
+              resizeMode: "contain",
+            }}      
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => console.log('Image clicked!')}>
+            <Image
+            source={require("../../assets/images/dashbordNavImg/Profile.png")}   
+            style={{
+              width: width * 0.140,
+              height: width * 0.140,
+              resizeMode: "contain",
+            }}     
+            />
+          </TouchableOpacity>    
+        </View>
+      </View>
+      {/* <CustomButton
         title="Login"
         onPress={() => router.replace("/(login)/loginScreen")}
         backgroundColor="#FCB647"
         textColor="white"
-      />
+      /> */}
     </SafeAreaView>
   );
 }
